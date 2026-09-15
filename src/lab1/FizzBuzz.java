0c04a95f5017c2e15d2f404408f5f40f731e3f78
Charlie Neale <charlieneale0@gmail.com>
2026-09-15 14:02:40 -0400

    Main.java can now call FizzBuzz.main. YIPPEEE

--- src/lab1/FizzBuzz.java
+++ src/lab1/FizzBuzz.java
@@ -15,7 +15,7 @@ class FizzBuzz {
         }
     }
 
-    private static void do_fizz_buzz(int i) {
+    public static void do_fizz_buzz(int i) {
         // Find out which numbers divide i.
         boolean divisibleBy3 = i % 3 == 0;
         boolean divisibleBy5 = i % 5 == 0;
