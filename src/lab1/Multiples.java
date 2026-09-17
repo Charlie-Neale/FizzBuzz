19d84bd8492b715ceb8b4384718b6344a2183280
Charlie Neale <charlieneale0@gmail.com>
2026-09-17 12:20:55 -0400

    Multiples working properly

---
+++ src/lab1/Multiples.java
@@ -0,0 +1,18 @@
+package lab1;
+
+public class Multiples {
+    static void main() {
+        int numer = multiples(6789,7,4);
+        System.out.println(numer);
+    }
+
+    public static int multiples(int n, int a, int b) {
+        int dingus = 0;
+        for (int i = 1; i < n; i++) {
+            if (i % a == 0 || i % b == 0) {
+                dingus++;
+            }
+        }
+        return dingus;
+    }
+}
